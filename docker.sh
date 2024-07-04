7e868148328652b1e129305b05961e2bd71748ac
Alfredo Godoy <agodoy@fi.uba.ar>
2024-07-04 04:20:41 -0300

    ruta psql actualizada

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo systemctl stop postgresql
-sudo docker run --rm -it -e POSTGRES_PASSWORD=123 -p 5432:5432 -v $(pwd)/backend/data:/var/lib/postgresql/data postgres:16
+sudo docker run --rm -it -e POSTGRES_PASSWORD=123 -p 5432:5432 -v $(pwd)/src/backend/data:/var/lib/postgresql/data postgres:16
