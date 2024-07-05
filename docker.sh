ec416cb0d7467e9d80c25202de986dc8b0fb7f9c
Sebastian Kupri <seebassnk@gmail.com>
2024-07-04 21:51:30 -0300

    Se agrega el repositorio apt de Docker para que no haya problemas al instalarlo

--- docker.sh
+++ docker.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
+
+# Instalacion de Docker. https://docs.docker.com/engine/install/ubuntu/
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
 #Si pasa algun error porque la base de datos no esta inicializada, ejecutar python3 main.py en la carpeta backend
 echo "Creando sesion docker con el puerto 5432 y data en src/backend/data"
 NOMBRE_CONTENEDOR='renaper' # Para identificar internamente al contenedor
-sudo systemctl stop postgresql # Docker no cierra automaticamente el servidor de backend
-sudo docker stop $NOMBRE_CONTENEDOR # Si no encuentra el contenedor sale una advertencia. ignorar
-sudo docker rm $NOMBRE_CONTENEDOR # si no encuentra el contenedor sale una advertencia. ignorar
+systemctl stop postgresql # Docker no cierra automaticamente el servidor de backend
+docker stop $NOMBRE_CONTENEDOR # Si no encuentra el contenedor sale una advertencia. ignorar
+docker rm $NOMBRE_CONTENEDOR # si no encuentra el contenedor sale una advertencia. ignorar
 ABRIR_POSTGRESQL="psql -U postgres"
 LISTAR_PERSONAS="SELECT * FROM personas;" #la \\ indica que son comandos internos del programa anterior
 
 
-sudo docker run --rm -d -it --name $NOMBRE_CONTENEDOR -e POSTGRES_PASSWORD=123 -p 5432:5432 -v $(pwd)/src/backend/data:/var/lib/postgresql/data postgres:16
-sudo docker exec -it $NOMBRE_CONTENEDOR bash -c "$ABRIR_POSTGRESQL -d renaper -c \"$LISTAR_PERSONAS\"" # Se cierra psql ni idea por qué 
+docker run --rm -d --name $NOMBRE_CONTENEDOR -e POSTGRES_PASSWORD=123 -p 5432:5432 -v $(pwd)/src/backend/data:/var/lib/postgresql/data postgres:16
+sleep 5 # Espera 5 segundos, para darle tiempo a docker a que inicialice todo lo necesario
 
-sudo docker exec -it $NOMBRE_CONTENEDOR bash -c "$ABRIR_POSTGRESQL -d renaper" # Para mantener psql abierto
+echo "-----------------------------------------------------------------"
+echo "Se ha iniciado correctamente una imagen de Docker con PostgreSQL."
+echo "-----------------------------------------------------------------"
+
+docker exec -it $NOMBRE_CONTENEDOR bash -c "$ABRIR_POSTGRESQL -d renaper" # Para mantener psql abierto
+docker stop  renaper
+
